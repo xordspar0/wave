935b3cfcf579386f15a60e90f3677abc191bf424
Jordan Christiansen <xordspar0@gmail.com>
2024-09-08 22:24:02 -0500

    pascal/highscores: Move SQLite persistence to a separate unit
    The normal game unit shouldn't need to know the details of how SQLite
    works.

--- pascal/highscores/game.pas
+++ pascal/highscores/game.pas
@@ -26,10 +26,9 @@ function ScoreGame(state : game.State) : Integer;
 implementation
 
 uses
-	sqldb,
-	sqlite3conn,
+	sdl2,
 
-	sdl2;
+	gamepersistence;
 
 function New() : State;
 var
@@ -62,63 +61,18 @@ begin
 end;
 
 function ScoreGame(state : game.State) : Integer;
-const
-	createTableGames = 'CREATE TABLE if not exists games (' +
-			'id integer primary key, sum integer, date timestamp DEFAULT CURRENT_TIMESTAMP' +
-		');';
-	createTableScores = 'CREATE TABLE if not exists scores (' +
-			'score integer, game_id integer, foreign key (game_id) references games(id)' +
-		');';
 var
-	conn   : TSQLite3Connection;
-	t      : TSQLTransaction;
-	q      : TSQLQuery;
-	i      : Smallint = 0;
-	sum    : Integer = 0;
-	gameId : Integer = 0;
+	i : Integer;
 begin
-	conn := TSQLite3Connection.Create(nil);
-	conn.DatabaseName := 'scores.db';
-	t := TSQLTransaction.Create(conn);
-	conn.Transaction := t;
-	conn.Open;
-
-	t.StartTransaction;
-	SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, '%s', [PChar(createTableGames)]);
-	conn.ExecuteDirect(createTableGames);
-	SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, '%s', [PChar(createTableScores)]);
-	conn.ExecuteDirect(createTableScores);
-	t.Commit;
-
-	t.StartTransaction;
+	ScoreGame := 0;
 
 	for i := Low(state.loot) to state.lootNum - 1 do
 	begin
-		sum := sum + state.loot[i].hue;
+		ScoreGame := ScoreGame + state.loot[i].hue;
 	end;
 
-	q := TSQLQuery.Create(nil);
-	q.Database := conn;
-	q.Transaction := t;
-	q.SQL.Text := 'INSERT INTO games (sum) VALUES (:sum) RETURNING id;';
-	q.Params.ParamByName('sum').AsInteger := sum;
-	SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, '%s [%d]', [PChar(q.SQL.Text), sum]);
-	q.Open;
-	gameId := q.FieldByName('id').AsInteger;
-	q.Close;
-
-	for i := Low(state.loot) to state.lootNum - 1 do
-	begin
-		q.SQL.Text := 'INSERT INTO scores (score, game_id) VALUES (:score, :game_id);';
-		q.Params.ParamByName('score').AsInteger := state.loot[i].hue;
-		q.Params.ParamByName('game_id').AsInteger := gameId;
-		SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, '%s [%d, %d]', [PChar(q.SQL.Text), state.loot[i].hue, gameId]);
-		q.ExecSQL;
-	end;
-
-	t.Commit;
-
-	ScoreGame := sum;
+	gamepersistence.PersistScore(state);
 end;
+
 
 end.
