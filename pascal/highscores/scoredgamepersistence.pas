0f9da5b28d110ab3c9efe89cf7c21583bd4e0370
Jordan Christiansen <xordspar0@gmail.com>
2024-09-21 14:05:47 -0500

    pascal/highscores: Use sqlite3 directly instead of SQLDB
    Free Pascal's SQLDB interface is more complicated than I need, and I
    didn't understand what it was doing internally. Something I was doing
    was leading to database lock errors whenever I tried to read from the
    database. The docs should have made it clear how to use the interface
    and what I was doing wrong, but the docs are pretty minimal.
    
    As a nice bonus, release builds are an order of magnitude smaller, down
    from ~1 MiB to ~100 KiB.

--- pascal/highscores/scoredgamepersistence.pas
+++ pascal/highscores/scoredgamepersistence.pas
@@ -12,134 +12,217 @@ function SelectTop5ScoredGames(var a : arenas.Arena) : ScoredGameList;
 implementation
 
 uses
-	sqldb,
-	sqlite3conn,
+	sqlite3,
+	sysutils,
 
 	sdl2;
 
-function Open() : TSQLite3Connection;
+function Open() : psqlite3;
 const
 	filename = 'scores.db';
 var
-	conn : TSQLite3Connection;
+	db     : psqlite3;
+	result : Integer;
 begin
-	conn := TSQLite3Connection.Create(nil);
-	conn.DatabaseName := filename;
-	conn.Transaction := TSQLTransaction.Create(conn);
-	conn.Open;
+	result := sqlite3_open(filename, @db);
+	if result <> SQLITE_OK then
+	begin
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, '%s', [sqlite3_errmsg(db)]);
+		Abort();
+	end;
 
-	Open := conn;
+	Open := db;
 end;
 
-procedure CreateTableGames(conn : TSQLite3Connection);
-const
-	createTableGames = 'CREATE TABLE if not exists games (' +
-			'id integer primary key, sum integer, date timestamp DEFAULT CURRENT_TIMESTAMP' +
-		');';
-	createTableGems = 'CREATE TABLE if not exists gems (' +
-			'value integer, game_id integer, foreign key (game_id) references games(id)' +
-		');';
+procedure Close(db : psqlite3);
 begin
-	conn.Transaction.StartTransaction;
+	sqlite3_close(db);
+end;
 
-	SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, '%s', [PChar(createTableGames)]);
-	conn.ExecuteDirect(createTableGames);
-	SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, '%s', [PChar(createTableGems)]);
-	conn.ExecuteDirect(createTableGems);
+procedure Exec(db : psqlite3; SQL : UTF8String);
+var
+	err : PChar;
+begin
+	SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, '%s', [PChar(SQL)]);
+	sqlite3_exec(db, PChar(SQL), Nil, Nil, @err);
+	if err <> Nil then
+	begin
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, '%s', [err]);
+		Abort();
+	end;
+end;
 
-	conn.Transaction.Commit;
+procedure BeginTransaction(db : psqlite3);
+const
+	SQL = 'begin transaction;';
+begin
+	Exec(db, SQL);
+end;
+
+procedure Commit(db : psqlite3);
+const
+	SQL = 'commit;';
+begin
+	Exec(db, SQL);
+end;
+
+procedure CreateTableGames(db : psqlite3);
+const
+	SQL = 'begin transaction; ' +
+		'CREATE TABLE if not exists games (' +
+			'id integer primary key, sum integer, date timestamp DEFAULT CURRENT_TIMESTAMP' +
+		'); ' +
+		'CREATE TABLE if not exists gems (' +
+			'value integer, game_id integer, foreign key (game_id) references games(id)' +
+		'); ' +
+		'commit;';
+begin
+	Exec(db, SQL);
 end;
 
 procedure InsertScoredGame(game : scoredgames.ScoredGame);
 var
-	conn   : TSQLite3Connection;
-	q      : TSQLQuery;
-	sum    : Integer = 0;
+	db     : psqlite3;
+	q      : psqlite3_stmt;
+	SQL    : UTF8String;
+	status : Integer = 0;
 	gameId : Integer = 0;
 	gem    : scoredGames.Gem;
 begin
-	conn := Open();
+	db := Open();
 
-	CreateTableGames(conn);
+	CreateTableGames(db);
 
-	conn.Transaction.StartTransaction;
+	BeginTransaction(db);
 
-	q := TSQLQuery.Create(nil);
-	q.Database := conn;
-	q.Transaction := conn.Transaction;
-	q.SQL.Text := 'INSERT INTO games (sum) VALUES (:sum) RETURNING id;';
-	q.Params.ParamByName('sum').AsInteger := sum;
-	SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, '%s [%d]', [PChar(q.SQL.Text), sum]);
-	q.Open;
-	gameId := q.FieldByName('id').AsInteger;
-	q.Close;
+	SQL := 'INSERT INTO games (sum) VALUES (:sum) RETURNING id;';
+	status := sqlite3_prepare_v2(db, PChar(SQL), Length(SQL), @q, Nil);
+	if status <> SQLITE_OK then
+	begin
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, '%s', [sqlite3_errmsg(db)]);
+		Abort();
+	end;
+
+	status := sqlite3_bind_int(q, 1, game.sum);
+	if status <> SQLITE_OK then
+	begin
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, '%s', [sqlite3_errmsg(db)]);
+		Abort();
+	end;
+
+	SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, '%s [%d]', [PChar(SQL), game.sum]);
+	if sqlite3_step(q) <> SQLITE_ROW then
+	begin
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, '%s', [sqlite3_errmsg(db)]);
+		Abort();
+	end;
+
+	gameId := sqlite3_column_int(q, 0);
+
+	sqlite3_finalize(q);
+
+	SQL := 'INSERT INTO gems (value, game_id) VALUES (:value, :game_id);';
+	status := sqlite3_prepare_v2(db, PChar(SQL), Length(SQL), @q, Nil);
+	if status <> SQLITE_OK then
+	begin
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, '%s', [sqlite3_errmsg(db)]);
+		Abort();
+	end;
 
 	for gem in game.gems do
 	begin
-		q.SQL.Text := 'INSERT INTO gems (value, game_id) VALUES (:value, :game_id);';
-		q.Params.ParamByName('value').AsInteger := gem;
-		q.Params.ParamByName('game_id').AsInteger := gameId;
-		SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, '%s [%d, %d]', [PChar(q.SQL.Text), gem, gameId]);
-		q.ExecSQL;
+		sqlite3_reset(q);
+
+		status := sqlite3_bind_int(q, 1, gem);
+		if status <> SQLITE_OK then
+		begin
+			SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, '%s', [sqlite3_errmsg(db)]);
+			Abort();
+		end;
+
+		status := sqlite3_bind_int(q, 2, gameId);
+		if status <> SQLITE_OK then
+		begin
+			SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, '%s', [sqlite3_errmsg(db)]);
+			Abort();
+		end;
+
+		SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, '%s [%d, %d]', [PChar(SQL), gem, gameId]);
+		if not sqlite3_step(q) in [SQLITE_ROW, SQLITE_DONE] then
+		begin
+			SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, '%s', [sqlite3_errmsg(db)]);
+			Abort();
+		end;
 	end;
 
-	conn.Transaction.Commit;
+	sqlite3_finalize(q);
+
+	Commit(db);
+	Close(db);
 end;
 
 function SelectTop5ScoredGames(var a : arenas.Arena) : ScoredGameList;
+const
+	selectFromGames = 'SELECT id, sum FROM games ORDER BY sum LIMIT 5;';
+	selectFromGems  = 'SELECT value FROM gems WHERE game_id = :game_id;';
 var
-	conn       : TSQLite3Connection;
-	gamesQuery : TSQLQuery;
-	gemsQuery  : TSQLQuery;
+	db         : psqlite3;
+	gamesQuery : psqlite3_stmt;
+	gemsQuery  : psqlite3_stmt;
+	status     : Integer = 0;
 	scores     : scoredgames.ScoredGameList;
 	i          : Integer = 0;
 	j          : Integer = 0;
-	gameId     : Integer;
+	gameId     : Integer = 0;
 begin
-	conn := Open();
-	CreateTableGames(conn);
+	db := Open();
+	CreateTableGames(db);
+
+	BeginTransaction(db);
 
 	scores.cap := 5;
 	scores.games := arenas.Alloc(a, SizeOf(scoredgames.ScoredGame), scores.cap);
 
-	gamesQuery := TSQLQuery.Create(nil);
-	gamesQuery.Database := conn;
-	gemsQuery := TSQLQuery.Create(nil);
-	gemsQuery.Database := conn;
-
-	with gamesQuery do
+	status := sqlite3_prepare_v2(db, PChar(selectFromGames), Length(selectFromGames), @gamesQuery, Nil);
+	if status <> SQLITE_OK then
 	begin
-		SQL.Text := 'SELECT * FROM games ORDER BY sum LIMIT 5;';
-		SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, '%s', [PChar(SQL.Text)]);
-		Open;
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, '%s', [sqlite3_errmsg(db)]);
+		Abort();
+	end;
 
-		while not EOF do
+	status := sqlite3_prepare_v2(db, PChar(selectFromGems), Length(selectFromGems), @gemsQuery, Nil);
+	if status <> SQLITE_OK then
+	begin
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, '%s', [sqlite3_errmsg(db)]);
+		Abort();
+	end;
+
+	SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, '%s', [PChar(selectFromGames)]);
+	while sqlite3_step(gamesQuery) <> SQLITE_DONE do
+	begin
+		gameId := sqlite3_column_int(gamesQuery, 0);
+		scores.games[i].sum := sqlite3_column_int(gamesQuery, 1);
+
+		sqlite3_reset(gemsQuery);
+		sqlite3_bind_int(gemsQuery, 1, gameId);
+		SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, '%s [%d]', [PChar(selectFromGems), gameId]);
+
+		j := 0;
+		while sqlite3_step(gemsQuery) <> SQLITE_DONE do
 		begin
-			gameId := FieldByName('id').AsInteger;
-			scores.games[i].sum := FieldByName('sum').AsInteger;
-
-			with gemsQuery do
-			begin
-				SQL.Text := 'SELECT * FROM gems WHERE game_id = :game_id;';
-				Params.ParamByName('game_id').AsInteger := gameId;
-				SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, '%s [%d]', [PChar(SQL.Text), gameId]);
-				Open;
-
-				while not EOF do
-				begin
-					scores.games[i].gems[j] := FieldByName('value').AsInteger;
-					Next;
-					Inc(j);
-				end;
-				Close;
-			end;
-
-			Next;
-			Inc(i);
+			scores.games[i].gems[j] := sqlite3_column_int(gemsQuery, 0);
+			Inc(j);
 		end;
 
-		Close;
+		Inc(i);
 	end;
+
+	sqlite3_finalize(gamesQuery);
+	sqlite3_finalize(gemsQuery);
+
+	Commit(db);
+
+	Close(db);
 
 	SelectTop5ScoredGames := scores;
 end;
